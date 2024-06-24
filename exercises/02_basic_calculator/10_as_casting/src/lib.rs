60ae52eee35fb98768634da2207dce6d9226c7b5
Bingle Kruger <bingle@deeplearning-cafe.com>
2024-06-24 17:03:20 +0200

    Basic calculator done

--- exercises/02_basic_calculator/10_as_casting/src/lib.rs
+++ exercises/02_basic_calculator/10_as_casting/src/lib.rs
@@ -6,17 +6,21 @@ mod tests {
 
     #[test]
     fn u16_to_u32() {
-        assert_eq!(47u16 as u32, todo!());
+        assert_eq!(47u16 as u32, 47u32);
     }
 
     #[test]
     #[allow(overflowing_literals)]
     fn u8_to_i8() {
-        assert_eq!(255 as i8, todo!());
+        #[allow(overflowing_literals)]
+        let x = { 255 as i8 };
+
+        let y: i8 = -1;
+        assert_eq!(x, y);
     }
 
     #[test]
     fn bool_to_u8() {
-        assert_eq!(true as u8, todo!());
+        assert_eq!(true as u8, 1u8);
     }
 }
